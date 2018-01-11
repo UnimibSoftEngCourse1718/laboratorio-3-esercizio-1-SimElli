ba6b1404affb05bb6056b58e41650492db5e8ba2
SimElli <s.elli@campus.unimib.it>
2018-01-12 00:26:03 +0100

    Used new library

--- quickstart/src/main/java/com/vogella/maven/quickstart/App.java
+++ quickstart/src/main/java/com/vogella/maven/quickstart/App.java
@@ -1,13 +1,16 @@
 package com.vogella.maven.quickstart;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    public static void main( String[] args ) {
+    	
+    	Gson gson = new Gson();
+    	System.out.print(gson.toJson("Hello World!"));
     }
 }
